6ca02fc32076aedd026906e04f662171ce626435
MiloshN <miloskm@live.com>
2019-06-11 20:22:46 +0200

    Work

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $("#show-label-1").on("click", function() {
+    $("#to-show-1").toggle("slow");
+    $(".toggle-1").toggleClass("toggle-rotate");
+  });
+  $("#show-label-2").on("click", function() {
+    $("#to-show-2").toggle("slow");
+    $(".toggle-2").toggleClass("toggle-rotate");
+  });
+  $("#show-label-3").on("click", function() {
+    $("#to-show-3").toggle("slow");
+    $(".toggle-3").toggleClass("toggle-rotate");
+  });
+  $("#show-label-4").on("click", function() {
+    $("#to-show-4").toggle("slow");
+    $(".toggle-4").toggleClass("toggle-rotate");
+  });
+  $("#show-label-5").on("click", function() {
+    $("#to-show-5").toggle("slow");
+    $(".toggle-5").toggleClass("toggle-rotate");
+  });
+  $("#show-label-6").on("click", function() {
+    $("#to-show-6").toggle("slow");
+    $(".toggle-6").toggleClass("toggle-rotate");
+  });
+});

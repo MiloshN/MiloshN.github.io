9dd50ea0f42ac1b3e1a39af8e0c97fbbdfd11318
MiloshN <miloskm@live.com>
2019-06-19 15:45:26 +0200

    wadawd

--- js/main.js
+++ js/main.js
@@ -56,19 +56,27 @@ $(document).ready(function() {
     ]
   });
 
-      $(".hamburger").on("click", function() {
-        $(".toggle-menu").slideToggle("slow", function() {
-          $(".hamburger").hide();
-          $(".cross").show();
-        });
-      });
+  $(".hamburger").on("click", function() {
+    $(".toggle-menu").slideToggle("slow", function() {
+      $(".hamburger").hide();
+      $(".cross").show();
+    });
+  });
 
-      $(".cross").on("click", function() {
-        $(".toggle-menu").slideToggle("slow", function() {
-          $(".cross").hide();
-          $(".hamburger").show();
-        });
-      });
+  $(".cross").on("click", function() {
+    $(".toggle-menu").slideToggle("slow", function() {
+      $(".cross").hide();
+      $(".hamburger").show();
+    });
+  });
 
-  baguetteBox.run(".references__gallery");
+  function openSpecGallery(i) {
+    galerija = baguetteBox.run(".references__gallery-hidden");
+    baguetteBox.show(0, galerija[i]);
+  }
+
+  $(".open-gallery").on("click", function() {
+    const index = $(this).data("open-gallery");
+    openSpecGallery(index);
+  });
 });

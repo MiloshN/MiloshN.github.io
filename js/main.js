c17f583869471fd4131775ee6e6d07e49c8d88c6
MiloshN <miloskm@live.com>
2019-06-21 10:31:55 +0200

    awdawda

--- js/main.js
+++ js/main.js
@@ -1,27 +1,17 @@
 $(document).ready(function() {
-  $("#show-label-1").on("click", function() {
-    $("#to-show-1").toggle("fast");
-    $(".toggle-1").toggleClass("toggle-rotate");
-  });
-  $("#show-label-2").on("click", function() {
-    $("#to-show-2").toggle("fast");
-    $(".toggle-2").toggleClass("toggle-rotate");
-  });
-  $("#show-label-3").on("click", function() {
-    $("#to-show-3").toggle("fast");
-    $(".toggle-3").toggleClass("toggle-rotate");
-  });
-  $("#show-label-4").on("click", function() {
-    $("#to-show-4").toggle("fast");
-    $(".toggle-4").toggleClass("toggle-rotate");
-  });
-  $("#show-label-5").on("click", function() {
-    $("#to-show-5").toggle("fast");
-    $(".toggle-5").toggleClass("toggle-rotate");
-  });
-  $("#show-label-6").on("click", function() {
-    $("#to-show-6").toggle("fast");
-    $(".toggle-6").toggleClass("toggle-rotate");
+  $(".show-label").each(function(i) {
+    $(this).on("click", function() {
+      $(".to-show").each(function(index) {
+        if (i === index) {
+          $(this).toggle("fast");
+        }
+      });
+      $(".toggle").each(function(indx) {
+        if (i === indx) {
+          $(this).toggleClass("toggle-rotate");
+        }
+      });
+    });
   });
   $(".partners__slider").slick({
     infinite: true,
@@ -62,12 +52,17 @@ $(document).ready(function() {
       $(".cross").show();
     });
   });
-
+  $(".hamburger").dblclick(function(e) {
+    e.preventDefault();
+  });
   $(".cross").on("click", function() {
     $(".toggle-menu").slideToggle("slow", function() {
       $(".cross").hide();
       $(".hamburger").show();
     });
+  });
+  $(".cross").dblclick(function(e) {
+    e.preventDefault();
   });
 
   function openSpecGallery(i) {

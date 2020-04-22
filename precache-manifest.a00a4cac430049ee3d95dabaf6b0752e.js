b46b122803895840243f0dd66b3593eaae4e531f
MiloshN <miloskm@live.com>
2020-04-22 21:09:31 +0200

    Init commit React

---
+++ precache-manifest.a00a4cac430049ee3d95dabaf6b0752e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dbe3d33825be47b6e67d25604611985",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6d59ddb309466f306a55",
+    "url": "./static/css/main.f34a0895.chunk.css"
+  },
+  {
+    "revision": "0096b6d951f2f907ba0b",
+    "url": "./static/js/2.a4cab624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a4cab624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d59ddb309466f306a55",
+    "url": "./static/js/main.99b49b73.chunk.js"
+  },
+  {
+    "revision": "3e3963b089e2b7456693",
+    "url": "./static/js/runtime-main.acd29812.js"
+  }
+]);
--- script.js
+++
@@ -1,7 +0,0 @@
-let button = document
-  .getElementsByClassName("toggle-nav-icon")[0]
-  .addEventListener("click", function() {
-    document
-      .getElementsByClassName("nav-list")[0]
-      .classList.toggle("toggle-active");
-  });
